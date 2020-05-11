91ac01491825772cbb0dc200fd14385d355a39a7
salazenas <salazenas@gmail.com>
2020-05-10 22:58:33 -0300

    Updates

---
+++ precache-manifest.f2ce78b0535ce14bb63d2c184531c023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3a1fabc6376bdafd1647cedf476ff4",
+    "url": "/salazenas.github.io/index.html"
+  },
+  {
+    "revision": "dcc1f7173e75024556ff",
+    "url": "/salazenas.github.io/static/css/main.205ee21d.chunk.css"
+  },
+  {
+    "revision": "e5af9c7a8105ece4c292",
+    "url": "/salazenas.github.io/static/js/2.5ba3becc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/salazenas.github.io/static/js/2.5ba3becc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcc1f7173e75024556ff",
+    "url": "/salazenas.github.io/static/js/main.e31412fc.chunk.js"
+  },
+  {
+    "revision": "87944ea3ba18696118da",
+    "url": "/salazenas.github.io/static/js/runtime-main.6df65bd4.js"
+  }
+]);
