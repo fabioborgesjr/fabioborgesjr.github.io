b50fed05fd1321b9618335e85402bf0fdcc87c35
salazenas <salazenas@gmail.com>
2020-08-30 22:31:52 -0300

    posts simple page

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -1,19 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { posts } from "../../modules/constants/PageConstants";
-import {
-  setActivePage
-} from '../../reducers/main';
+import { setActivePage } from "../../reducers/main";
+import Section from "../../components/section/Section";
+import "./Posts.scss";
 
 export default function Posts() {
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(setActivePage(posts));
   }, [dispatch]);
 
-  return (
-    <div>
-    </div>
-  );
+  return <Section className="posts">{"Em breve!"}</Section>;
 }
